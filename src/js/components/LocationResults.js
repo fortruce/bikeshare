01d930bf81dcf22321ca301f38f270e68d17c722
fortruce <>
2015-05-15 18:46:21 -0400

    make stations results link to walking directions from source to station

--- src/js/components/LocationResults.js
+++ src/js/components/LocationResults.js
@@ -3,6 +3,7 @@ var { State } = require('react-router');
 var Reflux = require('reflux');
 
 var BikeStore = require('../stores/BikeStore');
+var url = require('url');
 
 var { sortByDistance, kmToMi } = require('../utils/distance');
 
@@ -17,9 +18,19 @@ function roundDistance(distance) {
 
 var Station = React.createClass({
   render() {
+    var link = url.format({
+      protocol: 'https',
+      host: 'maps.google.com',
+      pathname: 'maps',
+      query: {
+        saddr: this.props.source,
+        daddr: this.props.state.lat + ',' + this.props.state.lng,
+        dirflg: 'w'
+      }
+    }).replace('%2B', '+').replace('%2C', ',');
     return (
       <div className="row">
-        <h4 className="small-12 column">{this.props.state.name}</h4>
+        <h4 className="small-12 column"><a href={link}>{this.props.state.name}</a></h4>
         <p className="small-6 column">Bikes: {this.props.state.nbBikes} / {this.props.state.nbBikes + this.props.state.nbEmptyDocks}</p>
         <p className="small-6 column align-right">{roundDistance(this.props.state.distance)} mi</p>
       </div>
@@ -47,7 +58,10 @@ var LocationResults = React.createClass({
   render() {
     var stations = sortByDistance(this.latLng(), this.state.stations).slice(0,10);
     stations = stations.map((s) => {
-      return (<Station state={s} key={s.id} />);
+      return (
+        <Station  source={this.getQuery().tracking ? 'Current+Location' : this.getParams().location}
+                  state={s}
+                  key={s.id} />);
     });
     return (
       <div className="row">
