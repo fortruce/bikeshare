1ee7709c1cef5bb72d077a90d6b226018079a6f4
fortruce <>
2015-07-07 10:05:28 -0400

    fix sizing on large screens

--- src/js/components/BodyContent.js
+++ src/js/components/BodyContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class BodyContent extends React.Component {
   render() {
     return (
-      <div className="col s12 l8 offset-l2 m10 offset-m1">
+      <div className="col s12 l6 offset-l3 m10 offset-m1">
         {this.props.children}
       </div>
     );
