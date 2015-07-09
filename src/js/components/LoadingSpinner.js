4041d6623200b239c3055a0ab730e5ecb828cf52
fortruce <>
2015-07-09 15:51:21 -0400

    fix circle preloader

--- src/js/components/LoadingSpinner.js
+++ src/js/components/LoadingSpinner.js
@@ -10,6 +10,12 @@ export default class LoadingSpinner extends React.Component {
             <div className="circle-clipper left">
               <div className="circle"></div>
             </div>
+            <div className="gap-patch">
+              <div className="circle"></div>
+            </div>
+            <div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
           </div>
         </div>
       </div>
