c4e1f9f5426d1759dba13bbf194a171c41d15cde
Joseph Rollins <rollins.joseph@gmail.com>
2015-07-11 15:00:45 -0400

    add required proptypes

--- src/js/components/TextIcon.js
+++ src/js/components/TextIcon.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class TextIcon extends React.Component {
+  static propTypes = {
+    text: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number
+          ]).isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
   render() {
     return (
       <div className="text-icon">
